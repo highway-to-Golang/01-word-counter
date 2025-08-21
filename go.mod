cfd4596a259c53ed17443491647ac775380109ba
Aleksandr Noskov <nemestny@politeh.ru>
2025-08-21 15:40:21 +0500

    Homework init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/highway-to-Golang/01-word-counter
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+)
+
+func main() {
+	filename := flag.String("file", "", "")
+	top := flag.Int("top", 10, "")
+	flag.Parse()
+
+	if *filename == "" {
+		fmt.Println("Usage: word-counter -file <filename> [-top N]")
+		os.Exit(1)
+	}
+
+	file, _ := os.Open(*filename)
+	defer file.Close()
+
+	wordCounts := make(map[string]int)
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		for _, word := range strings.Fields(scanner.Text()) {
+			clean := strings.ToLower(strings.TrimFunc(word, func(r rune) bool {
+				return !((r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9'))
+			}))
+			if clean != "" {
+				wordCounts[clean]++
+			}
+		}
+	}
+
+	var words []struct {
+		word  string
+		count int
+	}
+
+	for word, count := range wordCounts {
+		words = append(words, struct {
+			word  string
+			count int
+		}{word, count})
+	}
+
+	sort.Slice(words, func(i, j int) bool {
+		return words[i].count > words[j].count
+	})
+
+	if len(words) > *top {
+		words = words[:*top]
+	}
+
+	for _, w := range words {
+		fmt.Printf("%s: %d\n", w.word, w.count)
+	}
+}
